26cd20095245f9a10c360d30ca503d833e36c4d9
André Luiz Mendonça <andreluzz@gmail.com>
2019-05-20 16:20:34 -0300

    Refactoring

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -2,19 +2,11 @@ package notifier
 
 import (
 	"encoding/json"
-	"fmt"
 	"time"
 )
 
 type notification struct {
 }
-
-// Notification constants
-const (
-	NewsReceiverUsername  string = "receiver_username"
-	NewsReceiverGroup     string = "receiver_group"
-	NewsReceiverFollowers string = "receiver_followers"
-)
 
 // Message represents a notification
 type Message struct {
@@ -62,23 +54,31 @@ func (m *Message) BodyToStruct(object interface{}) {
 
 // Receiver represents
 type Receiver struct {
-	Users         []string `json:"users"`
-	Groups        []string `json:"groups"`
-	Followers     bool     `json:"followers"`
-	realtimeUsers []string
-	notifyUsers   []string
-	emailUsers    []string
+	Users     []string `json:"users"`
+	Groups    []string `json:"groups"`
+	Followers bool     `json:"followers"`
+	allUsers  []user
+}
+
+type user struct {
+	UserName     string
+	Email        bool
+	Notification bool
+	Realtime     bool
 }
 
 // LoadUsers get usernames from groups and followers
 func (r *Receiver) LoadUsers() {
 	// TODO: Get receivers usernames from groups and followers table
 	// TODO: Remove duplicated usernames
+	// TODO: Pupulate arrays: allUsers
 }
 
 // Validate get usernames from groups and followers
 func (r *Receiver) Validate() {
 	// TODO: Validate if users already have an equal notification
+	// TODO: Validate if user has email enabled and notification already sent email
+	// TODO: REmove users from notifyUsers and emailUsers
 }
 
 // New creates a new notification on the system
@@ -90,18 +90,15 @@ func New(message Message, receiver Receiver, passthrough, forceEmail bool) {
 		receiver.Validate()
 	}
 
-	for _, usr := range receiver.notifyUsers {
-		fmt.Println(usr)
-		// TODO: Insert one row for each user in the DB with the message
-	}
-
-	for _, usr := range receiver.emailUsers {
-		fmt.Println(usr)
-		// TODO: Send email
-	}
-
-	for _, usr := range receiver.realtimeUsers {
-		fmt.Println(usr)
-		// TODO: Emit to websocket
+	for _, usr := range receiver.allUsers {
+		if usr.Notification {
+			// TODO: Insert one row for each user in the DB with the message
+		}
+		if usr.Email {
+			// TODO: Send email
+		}
+		if usr.Realtime {
+			// TODO: Emit to websocket
+		}
 	}
 }
